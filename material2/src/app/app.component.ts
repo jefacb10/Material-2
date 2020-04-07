6bc10742c5d27e2bc0304d21497e7171c3c4aed1
jefacb10 <32496187+jefacb10@users.noreply.github.com>
2020-04-07 00:32:30 -0300

    progress-spinner

--- material2/src/app/app.component.ts
+++ material2/src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'material2';
-  notifications = 0;
+export class AppComponent implements OnInit {
+  constructor(private spinnerService: NgxSpinnerService) {
+  }
+
+  showSpinner = false;
+  ngOnInit() {
+    this.spinner();
+  }
+  spinner() {
+    this.spinnerService.show();
+    setTimeout(() => {
+      this.spinnerService.hide();
+    }, 3000);
+  }
+
+    loadData() {
+      this.showSpinner = true;
+      setTimeout(() => {
+        this.showSpinner = false;
+      }, 5000);
+    }
+
 }
--- material2/src/app/material/material.module.ts
+++ material2/src/app/material/material.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { MatIconModule } from '@angular/material/icon';
 
 import { MatBadgeModule } from '@angular/material/badge';
 
+import { NgxSpinnerModule } from "ngx-spinner";
+
 const MaterialComponents = [
   MatIconModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  NgxSpinnerModule
 ];
 
 @NgModule({
